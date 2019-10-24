90a3a9409b10ab3d7b1ebbeef1687be1bcd12c06
Jenke Scheen <j.scheen@sms.ed.ac.uk>
2019-10-24 12:38:03 +0100

    added sparse alternative results

--- plot_performances.py
+++ plot_performances.py
@@ -6,25 +6,29 @@ import numpy as np
 diffnet_run1 = pd.read_csv("diffnet_scaling/output/diffnet_runtime.csv", names=["nodes", "time"])
 diffnet_run1["Method"] = "DiffNet"
 
-lomap_druglike_light = pd.read_csv("lomap_scaling/output_cluster/lomap_druglike_light_1.csv", names=["nodes", "time"])
-lomap_druglike_light["Method"] = "LOMAP - Druglike light"
+diffnet_run2 = pd.read_csv("diffnet_scaling/output/diffnet_sparse_runtime.csv", names=["nodes", "time"])
+diffnet_run2["Method"] = "DiffNet - Sparse"
 
-lomap_druglike_heavy = pd.read_csv("lomap_scaling/output_cluster/lomap_druglike_heavy_1.csv", names=["nodes", "time"])
-lomap_druglike_heavy["Method"] = "LOMAP - Druglike heavy"
+# lomap_druglike_light = pd.read_csv("lomap_scaling/output_cluster/lomap_druglike_light_1.csv", names=["nodes", "time"])
+# lomap_druglike_light["Method"] = "LOMAP - Druglike light"
 
-lomap_bulky = pd.read_csv("lomap_scaling/output_cluster/lomap_bulky_1.csv", names=["nodes", "time"])
-lomap_bulky["Method"] = "LOMAP - bulky"
+# lomap_druglike_heavy = pd.read_csv("lomap_scaling/output_cluster/lomap_druglike_heavy_1.csv", names=["nodes", "time"])
+# lomap_druglike_heavy["Method"] = "LOMAP - Druglike heavy"
 
-lomap_fragments = pd.read_csv("lomap_scaling/output_cluster/lomap_fragment_1.csv", names=["nodes", "time"])
-lomap_fragments["Method"] = "LOMAP - fragments"
+# lomap_bulky = pd.read_csv("lomap_scaling/output_cluster/lomap_bulky_1.csv", names=["nodes", "time"])
+# lomap_bulky["Method"] = "LOMAP - bulky"
+
+# lomap_fragments = pd.read_csv("lomap_scaling/output_cluster/lomap_fragment_1.csv", names=["nodes", "time"])
+# lomap_fragments["Method"] = "LOMAP - fragments"
 
 
 df = pd.concat([
 				diffnet_run1,
-				lomap_druglike_light,
-				lomap_druglike_heavy,
-				lomap_bulky,
-				lomap_fragments
+				diffnet_run2,
+				# lomap_druglike_light,
+				# lomap_druglike_heavy,
+				# lomap_bulky,
+				# lomap_fragments
 				])
 
 
@@ -42,6 +46,7 @@ all_treated_nodes = np.arange(2, max(successful_nodes), 1)
 
 
 
-plt.savefig("lomap_vs_diffnet.png", dpi=300)
+plt.savefig("diffnet_types.png", dpi=300)
+#plt.savefig("lomap_vs_diffnet.png", dpi=300)
 
 plt.show()
